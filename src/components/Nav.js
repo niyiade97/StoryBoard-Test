2577f574b91079a976d4e7b46162ae276d56b14b
adeniyi <niyiade97@gmail.com>
2022-03-28 14:19:08 +0100

    made site nav active by default

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,7 +8,7 @@ const Nav = () => {
   return (
     <div className='border-b-2 mb-5 h-24 relative'>
         <div className='flex items-center absolute -bottom-0 left-0 mb-1.5'>
-          <NavLink to="/sites" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/sites" ? "border-primary text-primary" : ""}`}>My Sites</NavLink>
+          <NavLink to="/sites" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/sites" || "/" ? "border-primary text-primary" : ""}`}>My Sites</NavLink>
           <NavLink to="/users" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/users" ? "border-primary text-primary" : ""}`}>My Users</NavLink>
         </div>
     </div>
