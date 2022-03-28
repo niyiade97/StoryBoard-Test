38deffa48773118c65de2997432d2d849504458c
adeniyi <niyiade97@gmail.com>
2022-03-28 14:26:09 +0100

    made site nav active by default

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,11 +4,11 @@ import { useLocation } from 'react-router-dom'
 
 const Nav = () => {
   const location = useLocation();
-  console.log(location.pathname);
+  console.log(location.pathname === "/sites" || "/");
   return (
     <div className='border-b-2 mb-5 h-24 relative'>
         <div className='flex items-center absolute -bottom-0 left-0 mb-1.5'>
-          <NavLink to="/sites" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/sites" || "/" ? "border-primary text-primary" : ""}`}>My Sites</NavLink>
+          <NavLink to="/sites" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/sites" ? "border-primary text-primary" : "" || location.pathname === "/" ? "border-primary text-primary" : ""}`}>My Sites</NavLink>
           <NavLink to="/users" className={`py-3 pr-3 border-b-2 hover:text-primary ${location.pathname === "/users" ? "border-primary text-primary" : ""}`}>My Users</NavLink>
         </div>
     </div>
